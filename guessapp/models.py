034e445bfff717331ccfc58008d701a5ae37f50a
Martin Wilsdorf <de.marwils@gmail.com>
2022-08-11 01:28:24 +0200

    Add models for Wordlist and Word

--- guessapp/models.py
+++ guessapp/models.py
@@ -1,1 +1,18 @@
-# Create your models here.
+from django.db import models
+from django.db.models import CharField
+from django.db.models.functions import Length
+
+
+CharField.register_lookup(Length, "length")
+
+
+class Wordlist(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Word(models.Model):
+    wordlist = models.ForeignKey(
+        Wordlist, on_delete=models.CASCADE, related_name="words"
+    )
+    word = models.CharField(max_length=100)
+    rating = models.IntegerField(null=True)
--- guessapp/tests.py
+++ guessapp/tests.py
@@ -1,1 +1,13 @@
-# Create your tests here.
+import pytest
+
+from guessapp.models import Word, Wordlist
+
+
+@pytest.mark.django_db
+def test_filter_word_length():
+    german = Wordlist.objects.create(name="German")
+    german.words.create(word="abc")
+    german.words.create(word="abcd")
+    german.words.create(word="efgh")
+    assert 1 == Word.objects.filter(word__length=3).count()
+    assert 2 == Word.objects.filter(word__length=4).count()
