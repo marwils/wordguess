55b02cb8647c2c9d76b3ee63e1e85013e576f292
Martin Wilsdorf <de.marwils@gmail.com>
2022-08-17 00:42:53 +0200

    Fix filter word length test

--- guessapp/tests.py
+++ guessapp/tests.py
@@ -8,9 +8,9 @@ from guessapp.models import Word, Wordlist
 @pytest.mark.django_db
 def test_filter_word_length():
     german = Wordlist.objects.create(name="German")
-    german.words.create(word="abc")
-    german.words.create(word="abcd")
-    german.words.create(word="efgh")
+    german.words.create(word="abc", rating=6)
+    german.words.create(word="abcd", rating=7)
+    german.words.create(word="efgh", rating=4)
     assert 1 == Word.objects.filter(word__length=3).count()
     assert 2 == Word.objects.filter(word__length=4).count()
 
